2efe681c0ea3bf2c9b7d8ab3a65a4090d53791f7
rashokvk <rashokvk@gmail.com>
2021-12-19 23:45:39 +0530

    Build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export JEKYLL_ENV=production
+
+THEME_DIR=themes/affiliates-jekyll-theme
+SITE_REPO=../clickfortech.github.io
+SITE_REPO_FROM_THEME=../../../clickfortech.github.io
+
+# 1. Build
+cd ${THEME_DIR}
+bundle exec jekyll build
+
+# 2. Copy Site content
+if [ -d "${SITE_REPO_FROM_THEME}" ]; then
+    rm ${SITE_REPO_FROM_THEME}/* -rf
+    cp _site/* "${SITE_REPO_FROM_THEME}" -rf
+    echo "copied to ${SITE_REPO_FROM_THEME}"
+else
+    echo "[${SITE_REPO_FROM_THEME}] not found. So not copied"
+    pwd
+    ls -l ${SITE_REPO_FROM_THEME}
+fi
+
+cd ../..
+
+# 3. Copy README
+if [ -d "{SITE_REPO}" ]; then
+    cp assets/README.md ../${SITE_REPO}
+fi
