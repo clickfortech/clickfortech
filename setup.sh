b41c6f008384f42aa7b13b58345924426e8aa334
rashokvk <rashokvk@gmail.com>
2021-12-31 20:00:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+THEME_DIR=themes/affiliates-jekyll-theme
 # 1. Apply patches to theme
-cd themes/affiliates-jekyll-theme/
+cd ${THEME_DIR}
 git apply ../patches/config.patch
 git apply ../patches/about.patch
 cd ../../
 echo "Applied patches"
 
 # 2. Create symlink
-cd themes/affiliates-jekyll-theme/
+cd ${THEME_DIR}
+git apply ../patches/config.patch
 ln -s ../../_posts _posts
 cd assets
 ln -s ../../../assets/images images
